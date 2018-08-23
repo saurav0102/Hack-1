d776bb80749686904bd094af88fcb1858c4f86dd
skushwaha2 <42126850+skushwaha2@users.noreply.github.com>
2018-08-23 11:56:15 -0500

    change in code to add my name and major

--- hack1.c
+++ hack1.c
@@ -1,6 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
 int main(int argc, char **argv) {
-printf("Saurav Pradhan Computer Science\n");
+printf("Saurav Pradhan Computer Science\n saurav kushwaha computer science");
 return 0;
 }
